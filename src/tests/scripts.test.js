8325aa5c7c9cfd7f4f0ac44d543ae23e664d512e
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-08-04 19:11:10 -0300

    feat: validation test

---
+++ src/tests/scripts.test.js
@@ -0,0 +1,57 @@
+
+import { JSDOM } from 'jsdom'
+import fs from 'fs'
+import path from 'path'
+
+import { dbConection, createUser, nameValidation, emailValidation, cpfValidation, phoneValidation } from '../scripts'
+
+
+require("fake-indexeddb/auto");
+
+
+
+let dom
+let container
+
+describe('register user', () => {
+
+  const html = fs.readFileSync(path.resolve(__dirname,"../index.html"), 'utf8');
+
+  beforeEach(() => {
+    dom = new JSDOM(html, { runScripts: 'dangerously' })
+    container = dom.window.document.body
+  })
+
+  it('show message if not validated', () => {
+
+    document.body.innerHTML = `
+      <form id="user-form">
+        <input id="name-input" value="tt" />
+        <span id="name-span" style="visibility: hidden"><span/>
+        <input type="email" id="email-input" value="test" />
+        <span id="email-span" style="visibility: hidden"></span>
+        <input id="cpf-input"  value="test" pattern="([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})"/>
+        <span id="cpf-span" style="visibility: hidden"></span>
+        <input id="number-input" value="test" pattern="^\s*(\d{2}|\d{0})[-. ]?(\d{5}|\d{4})[-. ]?(\d{4})[-. ]?\s*$"/>
+        <span id="number-span" style="visibility: hidden"></span>
+        <button id="submit-button" type="submit"></button>
+      </form>
+    `;
+
+    const nameSpan = document.getElementById("name-span");
+    const emailSpan = document.getElementById("email-span");
+    const cpfSpan = document.getElementById("cpf-span");
+    const numberSpan = document.getElementById("number-span");
+
+    nameValidation();
+    emailValidation();
+    cpfValidation();
+    phoneValidation();
+
+    expect(nameSpan).toBeVisible();
+    expect(emailSpan).toBeVisible();
+    expect(cpfSpan).toBeVisible();
+    expect(numberSpan).toBeVisible();
+  })
+
+})
---
+++ src/tests/setupTests.js
@@ -0,0 +1,5 @@
+require("fake-indexeddb/auto");
+
+import 'regenerator-runtime/runtime'
+import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
