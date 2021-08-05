a8f85ff35b4704dfd028dd5cbb6600be3daf725c
NakajimaDaniel <nakajimadaniel96@gmail.com>
2021-08-05 16:05:17 -0300

    feat: create user unit test

--- src/tests/scripts.test.js
+++ src/tests/scripts.test.js
@@ -54,4 +54,36 @@ describe('register user', () => {
     expect(numberSpan).toBeVisible();
   })
 
+  it('create user if validated', async() => {
+
+    Date.now = jest.fn(() => 1487076708000)
+
+    document.body.innerHTML = `
+    <form id="user-form">
+      <input id="name-input" value="test" />
+      <span id="name-span" style="visibility: hidden"><span/>
+      <input type="email" id="email-input" value="test@test" />
+      <span id="email-span" style="visibility: hidden"></span>
+      <input id="cpf-input"  value="222.222.222-22" pattern="([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})"/>
+      <span id="cpf-span" style="visibility: hidden"></span>
+      <input id="number-input" value="11 1111 1111" pattern="^\s*(\d{2}|\d{0})[-. ]?(\d{5}|\d{4})[-. ]?(\d{4})[-. ]?\s*$"/>
+      <span id="number-span" style="visibility: hidden"></span>
+      <button id="submit-button" type="submit"></button>
+    </form>
+    `;
+
+    const userData = await createUser();
+
+    await expect(userData).toEqual({
+      "id": 1487076708000,
+      "name": "test",
+      "email": "test@test",
+      "cpf": "222.222.222-22",
+      "phoneNumber": "11 1111 1111"
+    })
+
+  })
+
+
+
 })
